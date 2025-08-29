e84cbf724bee99e813157fbbd23fb9b07f55a066
charlie <3140647@qq.com>
2025-08-29 15:27:34 +0800

    日期布局的文件写入

--- logger/rotate/date.go
+++ logger/rotate/date.go
@@ -1,19 +1,95 @@
 package rotate
 
-import "io"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
+)
 
-// ensure we always implement io.WriteCloser
-var _ io.WriteCloser = (*rotateDateWriter)(nil)
+type RotateDateWriter struct {
+	Filename string
+	Layout   string
 
-type rotateDateWriter struct {
-	MaxAge     int
-	MaxBackups int
+	time string
+	file *os.File
+	mu   sync.Mutex
 }
 
-func (l *rotateDateWriter) Write(p []byte) (n int, err error) {
-	return 0, nil
+func (r *RotateDateWriter) Write(p []byte) (n int, err error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if r.Layout == "" {
+		r.Layout = "2006-01-02"
+	}
+
+	date := time.Now().Format(r.Layout)
+	if r.file == nil || r.time != date {
+		if err := r.rotate(date); err != nil {
+			return 0, err
+		}
+	}
+
+	return r.file.Write(p)
 }
 
-func (l *rotateDateWriter) Close() error {
+func (r *RotateDateWriter) rotate(date string) error {
+	if err := r.close(); err != nil {
+		return err
+	}
+
+	dir := filepath.Dir(r.Filename)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	filename := r.filename(date)
+	fullpath := filepath.Join(dir, filename)
+	file, err := os.OpenFile(fullpath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+
+	r.file = file
+	return nil
+}
+
+func (r *RotateDateWriter) filename(date string) string {
+	defer func() {
+		r.time = date
+	}()
+
+	if r.Filename == "" {
+		return date
+	}
+
+	filename := filepath.Base(r.Filename)
+	if strings.HasPrefix(filename, ".") {
+		return filename + "." + date
+	}
+
+	ext := filepath.Ext(r.Filename)
+	name := filename[:len(filename)-len(ext)]
+
+	return name + "." + date + ext
+}
+
+func (r *RotateDateWriter) Close() error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	return r.close()
+}
+
+func (r *RotateDateWriter) close() error {
+	if r.file != nil {
+		if err := r.file.Close(); err != nil {
+			return err
+		}
+		r.file = nil
+	}
+
 	return nil
 }
